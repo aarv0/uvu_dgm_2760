03e7317631c578d7467b34936559ac422af08e2c
Ali <calicorn00@gmail.com>
2019-11-25 16:06:46 -0700

    Update

--- Unit04/js/main.js
+++ Unit04/js/main.js
@@ -54,31 +54,35 @@ function getFortune(fate) {
     let message
     switch (fate) {
         case 1: 
-          message: `be featured as the strongest superhero in the next Marvel movie!`
+          message = 'be featured as the strongest superhero in the next Marvel movie!'
           break
          case 2:
-           message: `win millions of dollars from a long lost uncle in Nigeria!`
+           message = 'win millions of dollars from a long lost uncle in Nigeria!'
            break
            case 3: 
-          message: `might be a winner winner chicken dinner!`
+          message = 'have all your hard work pay off!'
           break
          case 4:
-           message: `might be chased by a big, hungry bear!`
+           message = 'be chased by a big, hungry bear!'
            break
            case 5: 
-          message: `might be hugged by a stranger!`
+          message = 'have an important person will offer you support!'
           break
         default:
-           message: `Some sort of error occured` //Some sort of error occurred
+           message = 'Some sort of error occured' //Some sort of error occurred
            break
     }
+    return message
 }
 
 let fate = getRandomIntInclusive(1, 5)
 let day = getRandomIntInclusive(1, 30)
 let month = getRandomIntInclusive(1, 12)
 const monthName = getMonthName(month)
+const fortune = getFortune(fate)
 
-const fortuneRevealed = `On ${monthName} ${day}, you will  ${fate}` //template literal
+const fortuneRevealed = `On ${monthName} ${day}, you will ${fortune}` //template literal // fixed line when replaced {fate} with {fortune}
 
 document.querySelector('#fortune').innerText = fortuneRevealed
+
+console.log(fortune) // dont forget to add this
