2257aaa2bf5608514e9a55a3623836e94a5798ca
Ali <calicorn00@gmail.com>
2019-11-25 15:27:27 -0700

    Update

--- Unit04/js/main.js
+++ Unit04/js/main.js
@@ -4,10 +4,6 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; // maximum is inclusive and the minimum is inclusive
 }
 
-let month = getRandomIntInclusive(1, 12)
-let fate = getRandomIntInclusive(1, 5)
-let day = getRandomIntInclusive(1, 30)
-
 function getMonthName(month) {
  let name
  switch (month) {
@@ -16,6 +12,36 @@ function getMonthName(month) {
      break
     case 2:
      name = "February"
+     break
+     case 3:
+     name = "March"
+     break
+     case 4:
+     name = "April"
+     break
+     case 5:
+     name = "May"
+     break
+     case 6:
+     name = "June"
+     break
+     case 7:
+     name = "July"
+     break
+     case 8:
+     name = "August"
+     break
+     case 9:
+     name = "September"
+     break
+     case 10:
+     name = "October"
+     break
+     case 11:
+     name = "November"
+     break
+     case 12:
+     name = "December"
      break
     default:
      name = "Not a month"
@@ -28,13 +54,22 @@ function getFortune(fate) {
     let message
     switch (fate) {
         case 1: 
-          message: 'be featured as the strongest superhero in the next Marvel movie!'
+          message: `be featured as the strongest superhero in the next Marvel movie!`
           break
          case 2:
-           message: 'win millions of dollars from a long lost uncle in Nigeria'
+           message: `win millions of dollars from a long lost uncle in Nigeria!`
            break
+           case 3: 
+          message: `might be a winner winner chicken dinner!`
+          break
+         case 4:
+           message: `might be chased by a big, hungry bear!`
+           break
+           case 5: 
+          message: `might be hugged by a stranger!`
+          break
         default:
-           message: 'Some sort of error occured'
+           message: `Some sort of error occured` //Some sort of error occurred
            break
     }
 }
