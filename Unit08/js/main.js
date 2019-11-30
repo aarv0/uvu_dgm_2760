91d2042e7184f9a8083f46b456ac2c35ed528403
Ali <calicorn00@gmail.com>
2019-11-30 15:30:26 -0700

    Testing and update

--- Unit08/js/main.js
+++ Unit08/js/main.js
@@ -1,8 +1,8 @@
 // Document SetUp DOM
-document.querySelector('#company').innerText = "Pizza Emporium"
+document.querySelector('#company').innerText = "Grant's Tomb"
 document.querySelector('header > h2').innerText = "Literal Objects"
 
-let message
+
 
 const question = {
     stem: "Who is buried in Grant's Tomb?",
@@ -14,17 +14,29 @@ const question = {
     display: () => {
         document.querySelector('#stem').textContent = question.stem
         // display the question options here
+        document.querySelector('#answer1').textContent = question.option1
+        document.querySelector('#answer2').textContent = question.option2
+        document.querySelector('#answer3').textContent = question.option3
+        document.querySelector('#answer4').textContent = question.option4
+
+        
     },
     check: (userChoice) => {
-        if (userChoice === question.correct)
-        document.querySelector("feedback").textContent = "You are correct!"
+        if (userChoice === question.correct) {
+        document.querySelector(".feedback").textContent = "You are correct!"
+        // fixed ("feedback") and changed to (".feedback")
+        // document.querySelector(".feedback").style.backgroundColor  = "";
     }   // else...
+    else {
+        document.querySelector(".feedback").textContent = "Wrong answer. Try again?"
+        // document.querySelector(".feedback").style.backgroundColor  = "";
+    }
+    }
 }
-
-document.querySelector('#answer1').addEventListener('click,' () => question.check(1))
-document.querySelector('#answer2').addEventListener('click,' () => question.check(2))
-document.querySelector('#answer3').addEventListener('click,' () => question.check(3))
-document.querySelector('#answer4').addEventListener('click,' () => question.check(4))
+document.querySelector('#answer1').addEventListener('click', () => question.check(1))
+document.querySelector('#answer2').addEventListener('click', () => question.check(2))
+document.querySelector('#answer3').addEventListener('click', () => question.check(3))
+document.querySelector('#answer4').addEventListener('click', () => question.check(4))
 
 
 question.display()
