c4aeed7cdb544e51f6e1872ad6c9b47bebbe0282
Ali <calicorn00@gmail.com>
2019-12-01 23:59:26 -0700

    Testing and updates

---
+++ Unit11/js/main.js
@@ -0,0 +1,51 @@
+async function getHotelData() {
+    try {
+        const response = await fetch('../hotel.json')
+        return await response.json() // Return the JSON object
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+
+
+// store in a variable document.querySelectorAll("a")
+// use that variable to loop
+document.querySelector('#marriott').addEventListener('click', hotelInfo)
+document.querySelector('#marriott').addEventListener('click', hotelInfo)
+document.querySelector('#marriott').addEventListener('click', hotelInfo)
+
+//function hotelInfo(event) {
+//    console.log(event.target.id)
+//}
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    console.log(hotelChoice)
+
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+
+    document.querySelector("#address").textContent = `${hotelChoice.address}`
+
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
+
+    document.querySelector("#gym").textContent = `${hotelChoice.gym}`
+
+    document.querySelector("#type").textContent = `${hotelChoice.roomTypes}`
+
+    let imagePath = `${hotelChoice.picture}`
+
+    document.querySelector("#picture").setAttribute('src', imagePath)
+
+}
+
+//hotelData = data
+//console.log (hotelData)
+// console.log (hotelData) // returns an empty object
+// console.log(getHotelData().then(data => {
+    //return hotelData = data
+//}))
+// console.log(hotelData)
