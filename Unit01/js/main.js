52cafce84534fa3c4f70f66191a6d2939bc07f81
Ali <calicorn00@gmail.com>
2019-12-09 23:53:35 -0700

    Update

--- Unit01/js/main.js
+++ Unit01/js/main.js
@@ -7,6 +7,13 @@ document.querySelector('header > h2').innerText = "Best B&B this side of Orem!"
 
 let userName = prompt("What is your name?")
 
+
+var askforName = prompt("Enter Your Name");
+
+
+var myCup = "Welcome " + askforName;
+document.getElementById("whatisyourname").innerHTML = myCup;
+
 //let message = "Hello" + userName + ", welcome to the best lodging in Utah"
 
 let message = `Hello ${userName}, welcome to the finest lodging  in Utah County`
